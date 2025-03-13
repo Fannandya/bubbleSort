84b5a76b53ebaf18f01dc73726df1367cbcc8e74
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-03-13 11:52:31 +0700

    menampilkan array

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -44,6 +44,21 @@ void bubbleSortArray(){
                 arr[j+1] = temp;
             }
         }
+        pass = pass + 1;// step 4
     } while (pass <= n-1);
     
+}
+
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j + 0; j < n; j++){
+        cout << arr[j]; // menampilkan array
+        if (j < n - 1){
+            cout << "--->";
+        }
+    }
 }
