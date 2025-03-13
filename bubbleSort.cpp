97af55c0d7e027695a8c02655f25ce4627eda096
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-03-13 11:29:32 +0700

    pembuatan prosedur input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,4 +5,20 @@ using namespace std;
 int arr[20];        // deklarasi variabel global array dengan ukuran 20
 int n;              // deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
 
+void input(){       // prosedur untuk input
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : "<< endl;   // output ke layar
+        cin >> n;       // input dari pengguna
+        if (n <=20)
+            break;
+        else
+        {
+           cout << "\nArray dapat mempunyai maksimal 20 elemen\n" << endl;
+        }
+        
+    }
+    
 
+}
